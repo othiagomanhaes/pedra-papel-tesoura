4eaaf480aa744a2e78a348e2f61ba197a5c74e23
othiagomanhaes <othiagomanhaes@gmail.com>
2023-09-05 10:47:13 -0300

    tipagem do app.ts e correção das portas para o servidor responder.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, { Application, Request, Response } from 'express';
 
-const app = express();
+const app: Application = express();
 
 app.use(express.json());
 
-app.get('/', (_req, res) => {
+
+app.get('/', async (_req: Request, res: Response) => {
   res.status(200).json({ message: 'To servindo bem' })
 });
 
